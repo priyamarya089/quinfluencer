4fcfc86d26d027e103d0fa9d5851a1372a6ae700
priyam <priyam@oncequince.com>
2024-03-15 14:48:36 +0530

    version updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,21 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+buildscript {
+
+    val kotlinVersion = "1.8.0"
+    val springBootVersion = "3.0.1"
+
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.bmuschko:gradle-docker-plugin:9.1.0")
+    }
+}
+
 repositories {
     mavenCentral()
 }
